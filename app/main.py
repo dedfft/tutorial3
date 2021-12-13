51a6bb1f9177a19ad5af6a3c440b5999304986d7
dedfft <43168887+dedfft@users.noreply.github.com>
2021-12-13 16:19:48 +0300

    init

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ def create_database(app):
         print("Created database!")
 
 
-app = create_app()
+# app = create_app()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=80,debug=True)
