7abb624f9724dbf715ca5617e0c11b6a913c609d
dedfft <43168887+dedfft@users.noreply.github.com>
2021-12-13 16:01:51 +0300

    init

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from ttt import create_app
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0",port=80,debug=True)
+
+app = create_app()
+app.run(host="0.0.0.0",port=80,debug=True)
